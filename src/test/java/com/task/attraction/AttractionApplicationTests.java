a09379d9e985e5b76c823964285ed48ce695d7ad
ereminsasha08 <SashaEremin08051998@gmail.com>
2023-02-01 11:29:09 +0500

    init

---
+++ src/test/java/com/task/attraction/AttractionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.task.attraction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttractionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
